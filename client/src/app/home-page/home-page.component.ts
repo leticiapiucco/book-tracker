7d155351a78578276f09191d31e5312a7c3084fb
Leticia Piucco <leticia.piucco@gmail.com>
2024-10-16 20:52:15 +0100

    Add search results page

--- client/src/app/home-page/home-page.component.ts
+++ client/src/app/home-page/home-page.component.ts
@@ -1,38 +1,19 @@
 import { Component } from '@angular/core';
 import { BooksService } from '../services/books.service';
-import { map } from 'rxjs/operators';
-import { Observable } from 'rxjs';
 import { AsyncPipe, JsonPipe, NgFor } from '@angular/common';
-
-export interface Book{
-  title: string,
-  authors: Array<string>,
-  publisher: string, 
-  publishedDate: string,
-  description: string,
-  imageLinks: { 
-    "smallThumbnail":string, 
-    "thumbnail":string,
-  },
-  id: string
-
-}
+import { SearchResultsComponent } from "../search-results/search-results.component";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
   imports: [NgFor,
     AsyncPipe,
-    JsonPipe
-  ],
+    JsonPipe, SearchResultsComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
-  books$: Observable<Book[]>;
   constructor(private booksService : BooksService){
-    this.books$=this.booksService.search().pipe(map(res => res))
-    console.log(this.books$)
   }
 
   ngOnInit(){
